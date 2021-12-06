b0a58fc43ab8b139a0a287a7aeec6750ddc8ce14
Rafael Sandrini <rafael@sandrini.com.br>
2021-12-06 18:46:27 -0300

    Double advantage

--- discord_client.py
+++ discord_client.py
@@ -18,6 +18,7 @@ ALTERNATIVE_COMMAND_CHAR = ENV['alternative_char']
 COMMAND_HELPER = ENV['command_helper']
 
 COMMAND_ROLL = ENV['command_roll']
+COMMAND_ROLL_DOUBLE_ADVANTAGE = ENV["command_roll_double_advantage"]
 COMMAND_ROLL_ADVANTAGE = ENV["command_roll_advantage"]
 COMMAND_ROLL_DISADVANTAGE = ENV["command_roll_disadvantage"]
 
@@ -150,40 +151,72 @@ async def calculate_dices(dices_positive, dices_negative, additional):
             "dies_verbose": dies_verbose}
 
 
-async def reroll_and_send_text(context, dices_data=None, adv=True):
-    dice_result = await roll_dice(2, 20)
+async def reroll_and_send_text(context, dices_data=None, adv=True, number_of_dices=2):
+    if number_of_dices > 3:
+        number_of_dices = 3
+    dice_result = await roll_dice(number_of_dices, 20)
 
     try:
         text = f"{context.message.author.display_name}: 1d20 => "
     except:
         text = f" 1d20 => "
 
-    dice_1 = dice_2 = ""
+    dice_1 = dice_2 = dice_3 = ""
     if dice_result[0] == 20 or dice_result[0] == 1:
         dice_1 = "!"
     if dice_result[1] == 20 or dice_result[1] == 1:
         dice_2 = "!"
+    if number_of_dices == 3:
+        if dice_result[2] == 20 or dice_result[2] == 1:
+            dice_3 = "!"
 
     if adv:
-        if dice_result[0] >= dice_result[1]:
-            text += f"[ {dice_result[0]}{dice_1},  ~~{dice_result[1]}{dice_2}~~ ]"
-            result = dice_result[0]
+        if number_of_dices == 2:
+            if dice_result[0] >= dice_result[1]:
+                text += f"[ {dice_result[0]}{dice_1},  ~~{dice_result[1]}{dice_2}~~ ]"
+                result = dice_result[0]
+            else:
+                text += f"[ ~~{dice_result[0]}{dice_1}~~, {dice_result[1]}{dice_2} ]"
+                result = dice_result[1]
         else:
-            text += f"[ ~~{dice_result[0]}{dice_1}~~, {dice_result[1]}{dice_2} ]"
-            result = dice_result[1]
-    else:
-        if dice_result[0] <= dice_result[1]:
-            text += f"[ {dice_result[0]}{dice_1}, ~~{dice_result[1]}{dice_2}~~ ]"
-            result = dice_result[0]
-        else:
-            text += f"[ ~~{dice_result[0]}{dice_1}~~, {dice_result[1]}{dice_2} ]"
-            result = dice_result[1]
+            if dice_result[0] >= dice_result[1] and dice_result[0] >= dice_result[2]:
+                text += f"[ {dice_result[0]}{dice_1},  ~~{dice_result[1]}{dice_2}~~,  ~~{dice_result[2]}{dice_3}~~ ]"
+                result = dice_result[0]
+            elif dice_result[1] >= dice_result[0] and dice_result[1] >= dice_result[2]:
+                text += f"[ ~~{dice_result[0]}{dice_1}~~, {dice_result[1]}{dice_2}, ~~{dice_result[2]}{dice_3}~~, ]"
+                result = dice_result[1]
+            else:
+                text += f"[ ~~{dice_result[0]}{dice_1}~~, ~~{dice_result[1]}{dice_2}~~, {dice_result[2]}{dice_3} ]"
+                result = dice_result[2]
 
-    if dice_result[0] == 1 and dice_result[1] == 1 or dice_result[0] == 20 and dice_result[1] == 20:
-        text += f" ¯\_(ツ)_/¯ \n"
+    else:
+        if number_of_dices == 2:
+            if dice_result[0] <= dice_result[1]:
+                text += f"[ {dice_result[0]}{dice_1}, ~~{dice_result[1]}{dice_2}~~ ]"
+                result = dice_result[0]
+            else:
+                text += f"[ ~~{dice_result[0]}{dice_1}~~, {dice_result[1]}{dice_2} ]"
+                result = dice_result[1]
+        else:
+            if dice_result[0] <= dice_result[1] and dice_result[0] <= dice_result[2]:
+                text += f"[ {dice_result[0]}{dice_1}, ~~{dice_result[1]}{dice_2}~~, ~~{dice_result[2]}{dice_3}~~ ]"
+                result = dice_result[0]
+            elif dice_result[1] <= dice_result[0] and dice_result[1] <= dice_result[3]:
+                text += f"[ ~~{dice_result[0]}{dice_1}~~, {dice_result[1]}{dice_2}, ~~{dice_result[2]}{dice_3}~~ ]"
+                result = dice_result[1]
+            else:
+                text += f"[ ~~{dice_result[0]}{dice_1}~~, ~~{dice_result[1]}{dice_2}~~, {dice_result[2]}{dice_3} ]"
+                result = dice_result[2]
+
+    if number_of_dices == 2:
+        if dice_result[0] == 1 and dice_result[1] == 1 or dice_result[0] == 20 and dice_result[1] == 20:
+            text += f" ¯\_(ツ)_/¯ \n"
+    else:
+        if dice_result[0] == 1 and dice_result[1] == 1 and dice_result[2] == 1 or dice_result[0] == 20 and dice_result[1] == 20 and dice_result[2] == 20:
+            text += f" ¯\_(ツ)_/¯ HOLY FUCKING! Pode subir um nivel, isso é impossivel, sério mesmo, fala com o mestre e sobe um nivel, vc é o Deus dos dados.\n"
 
     extra_signal = "+" if dices_data and not "+" in dices_data and not "-" in dices_data else ""
-    final_text = f"{result}{extra_signal}{dices_data}="
+    final_text = f"{result}{extra_signal}{dices_data}=" if dices_data else ""
     result_final = result+eval(dices_data) if dices_data else result
 
     print(f"{text} \n{final_text} **{result_final}**")
@@ -241,6 +274,18 @@ async def command_roll_dices(context, data):
 async def command_roll_advantage_dices(context, data=None):
     try:
         await reroll_and_send_text(context, data, True)
+    except Exception as e:
+        await context.send(
+            f"Comando nao reconhecido, use: {COMMAND_CHAR}{COMMAND_ROLL_ADVANTAGE} +2 por exemplo")
+        await context.send(f"Exception {e}")
+
+@bot.command(
+    name=COMMAND_ROLL_DOUBLE_ADVANTAGE,
+    description="Roll dices with double advantage?"
+)
+async def command_roll_double_advantage_dices(context, data=None):
+    try:
+        await reroll_and_send_text(context, data, True, 3)
     except Exception as e:
         await context.send(
             f"Comando nao reconhecido, use: {COMMAND_CHAR}{COMMAND_ROLL_ADVANTAGE} +2 por exemplo")
